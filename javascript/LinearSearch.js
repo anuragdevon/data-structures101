be6e26a66e8875c56f9e88942ad9bab7b6dbd1c7
Yash Karanke <yashk@nuv.ac.in>
2020-10-08 18:05:32 +0530

    Linear Search
    Added Linear Search in JavaScript

---
+++ javascript/LinearSearch.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Array}
+ * @param {String}
+ * @author Yash Karanke
+ * */
+const linearSearch = (arr, elToFind) => {
+  for (let i=0; i<arr.length; i++) {
+    if (arr[i] == elToFind) {
+      return i;
+    }
+  } return null;
+}
+
+
+// Usage
+let rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+console.log(linearSearch(rainbow, "green")); // returns 3
+console.log(linearSearch(rainbow, "white")); // returns null
