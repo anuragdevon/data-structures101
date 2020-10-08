824e9fa7f360cdada18d30b72ccfea26492837cc
Yash Karanke <yashk@nuv.ac.in>
2020-10-08 10:14:16 +0530

    Insertion SOrt
    Implemented Insertion Sort in JavaScript

---
+++ javascript/InsertionSort.js
@@ -0,0 +1,19 @@
+const insertionSort = (inputArr) => {
+    let n = inputArr.length;
+        for (let i = 1; i < n; i++) {
+            // Choosing the first element in our unsorted subarray
+            let current = inputArr[i];
+            // The last element of our sorted subarray
+            let j = i-1; 
+            while ((j > -1) && (current < inputArr[j])) {
+                inputArr[j+1] = inputArr[j];
+                j--;
+            }
+            inputArr[j+1] = current;
+        }
+    return inputArr;
+}
+// Usage
+let inputArr = [5, 2, 4, 6, 1, 3];
+insertionSort(inputArr);
+console.log(inputArr);
