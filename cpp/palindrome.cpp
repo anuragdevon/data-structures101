7ab5fa3fc249a8253ac46cdbbfeceb81d7cbc87a
Arpit Arun Kumaar <arpitxk@outlook.com>
2022-10-11 16:08:03 -0400

    palindrome series in c++

---
+++ cpp/palindrome.cpp
@@ -0,0 +1,15 @@
+#include <iostream>  
+using namespace std;  
+int main() {  
+  int n1=0,n2=1,n3,i,num;    
+  cout << "Enter number of elements: ";    
+  cin >> num;    
+  cout << n1 << " " << n2 << " ";    
+  for(i=2; i<num; ++i) {    
+    n3 = n1+n2;    
+    cout << n3 << " ";    
+    n1 =n 2;    
+    n2 = n3;    
+  }    
+ return 0;  
+}  
